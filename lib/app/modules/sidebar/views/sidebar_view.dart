75b3185435d2c2ea6dca8323bcad5974c2c60bb6
JohnObama24 <johnobama24keren@gmail.com>
2024-09-11 12:15:24 +0700

    make a sidebar

--- lib/app/modules/sidebar/views/sidebar_view.dart
+++ lib/app/modules/sidebar/views/sidebar_view.dart
@@ -4,11 +4,8 @@ import '../../../../constanta/app_colors.dart';
 import 'package:construx_beta/app/modules/sidebar/controllers/sidebar_controller.dart';
 
 class Sidebar extends StatelessWidget {
-
   final SidebarController controller = Get.put(SidebarController());
-   Sidebar({super.key});
-
-
+  Sidebar({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,114 +26,159 @@ class Sidebar extends StatelessWidget {
               ],
             ),
           ),
-         Obx(() => ListTile(
-            leading: const Icon(Icons.home, color: Colors.white,),
-            title: Text('Home', style: TextStyle(color: Colors.white),),
-            tileColor: controller.selectedIndex.value == 0
-                ? Colors.green
-                : AppColors.abu,
-            onTap: () {
-              controller.selectItem(0);
-              Get.toNamed('/home');
-            },
+          Obx(() => Container(
+            color: controller.selectedIndex.value == 0
+                      ? Colors.green
+                      : AppColors.abu,
+            child: ListTile(
+                  leading: const Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    controller.selectItem(0);
+                    // Reset sub item selection when main item is selected
+                    controller.selectSubItem(-1); // Add this line to reset sub items
+                    Get.toNamed('/home');
+                  },
+                ),
           )),
-                    
           Obx(() => ExpansionTile(
-            leading: const Icon(Icons.settings, color: Colors.white,),
-            title: Text('basic settings', style: TextStyle(color: Colors.white),),
-            tilePadding: EdgeInsets.symmetric(horizontal: 16),
-            collapsedBackgroundColor: Colors.transparent,
-            backgroundColor: controller.selectedIndex.value == 1
-                ? Colors.green
-                : Colors.transparent,
-            initiallyExpanded: controller.isExpanded(1),
-            onExpansionChanged: (bool expanded) {
-              controller.toggleExpand(1);
-              if (expanded) {
-                controller.selectItem(1);
-              } else {
-                // Jangan reset selectedIndex ketika collapsed
-                if (controller.selectedIndex.value == 1) {
-                  controller.selectItem(-1);
-                }
-              }
-            },
-            children: <Widget>[
-              Container( // Wrap subitems with a Container for consistent background
-                color: controller.selectedSubIndex.value == 1
-                    ? Colors.white
-                    : AppColors.abu,
-                child: ListTile(
-                  contentPadding: const EdgeInsets.only(left: 50.0),
-                  leading: Icon(Icons.info, color: controller.selectedSubIndex.value == 1
-                    ? Colors.black
-                    : Colors.white,),
-                  title: Text(
-                      'Company Information',
-                      style: TextStyle(
-                        color: controller.selectedSubIndex.value == 1 ? Colors.black : Colors.white,
+                leading: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Basic Settings',
+                  style: TextStyle(color: Colors.white),
+                ),
+                tilePadding: EdgeInsets.symmetric(horizontal: 16),
+                collapsedBackgroundColor: Colors.transparent,
+                backgroundColor: controller.selectedIndex.value == 1
+                    ? Colors.green
+                    : Colors.transparent,
+                initiallyExpanded: controller.isExpanded(1),
+                onExpansionChanged: (bool expanded) {
+                  controller.toggleExpand(1);
+                  if (expanded) {
+                    controller.selectItem(1);
+                  }
+                },
+                children: <Widget>[
+                  Container(
+                    color: controller.selectedSubIndex.value == 1
+                        ? Colors.white
+                        : AppColors.abu,
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.only(left: 50.0),
+                      leading: Icon(
+                        Icons.info,
+                        color: controller.selectedSubIndex.value == 1
+                            ? Colors.black
+                            : Colors.white,
                       ),
+                      title: Text(
+                        'Company Information',
+                        style: TextStyle(
+                          color: controller.selectedSubIndex.value == 1
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                      tileColor: controller.selectedSubIndex.value == 1
+                          ? Colors.white
+                          : Colors.transparent,
+                      onTap: () {
+                        controller.selectSubItem(1);
+                        Get.toNamed('/company-information');
+                      },
                     ),
+                  ),
+                  Container(
+                    color: controller.selectedSubIndex.value == 2
+                        ? Colors.white
+                        : AppColors.abu,
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.only(left: 50.0),
+                      leading: Icon(
+                        Icons.person,
+                        color: controller.selectedSubIndex.value == 2
+                            ? Colors.black
+                            : Colors.white,
+                      ),
+                      title: Text(
+                        'User Role',
+                        style: TextStyle(
+                          color: controller.selectedSubIndex.value == 2
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                      tileColor: controller.selectedSubIndex.value == 2
+                          ? Colors.white
+                          : Colors.transparent,
+                      onTap: () {
+                        controller.selectSubItem(2);
+                        Get.toNamed('/user-role');
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: controller.selectedSubIndex.value == 3
+                        ? Colors.white
+                        : AppColors.abu,
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.only(left: 50.0),
+                      leading: Icon(
+                        Icons.category_outlined,
+                        color: controller.selectedSubIndex.value == 3
+                            ? Colors.black
+                            : Colors.white,
+                      ),
+                      title: Text(
+                        'Commodity Category',
+                        style: TextStyle(
+                          color: controller.selectedSubIndex.value == 3
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                      tileColor: controller.selectedSubIndex.value == 3
+                          ? Colors.white
+                          : Colors.transparent,
+                      onTap: () {
+                        controller.selectSubItem(3);
+                        Get.toNamed('/commodity-category');
+                      },
+                    ),
+                  ),
+                ],
+              )),
 
-                  tileColor: controller.selectedSubIndex.value == 1
-                    ? Colors.white
-                    : Colors.transparent,
-                  onTap: () {
-                    controller.selectSubItem(1);
-                    Get.toNamed('/company-information');
-                  },
-                ),
-              ),
-              Container(
-                color: controller.selectedSubIndex.value == 2
-                    ? Colors.white
+              Obx( () => Container(
+                color: controller.selectedIndex.value == 2
+                    ? Colors.green
                     : AppColors.abu,
                 child: ListTile(
-                  contentPadding: const EdgeInsets.only(left: 50.0),
-                  leading: Icon(Icons.person, color: controller.selectedSubIndex.value == 2
-                    ? Colors.black
-                    : Colors.white,),
-                  title: Text(
-                    'User Role',
-                    style: TextStyle(
-                      color: controller.selectedSubIndex.value == 2 ? Colors.black : Colors.white,
-                    ),
+                  leading: const Icon(
+                    Icons.notifications,
+                    color: Colors.white,
                   ),
-                  tileColor: controller.selectedSubIndex.value == 2
-                    ? Colors.white
-                    : Colors.transparent,
+                  title: Text(
+                    'Receiving Management',
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onTap: () {
-                    controller.selectSubItem(2);
-                    Get.toNamed('/user-role');
+                    controller.selectItem(2);
+                    controller.selectSubItem(-1); 
+                    Get.toNamed('/receiving-management');
                   },
                 ),
-              ),
-              Container(
-                color: controller.selectedSubIndex.value == 3
-                    ? Colors.white
-                    : AppColors.abu,
-                child: ListTile(
-                  contentPadding: const EdgeInsets.only(left: 50.0),
-                  leading: Icon(Icons.category_outlined, color: controller.selectedSubIndex.value == 3
-                    ? Colors.black
-                    : Colors.white,),
-                  title: Text(
-                    'Commodity Category',
-                    style: TextStyle(
-                      color: controller.selectedSubIndex.value == 3 ? Colors.black : Colors.white,
-                    ),
-                  ),
-                  tileColor: controller.selectedSubIndex.value == 3
-                    ? Colors.white
-                    : Colors.transparent,
-                  onTap: () {
-                    controller.selectSubItem(3);
-                    Get.toNamed('/commodity-category');
-                  },
-                ),
-              ),
-            ],
-          )),
+              ))
         ],
       ),
     );
