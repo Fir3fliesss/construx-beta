7ee14f80956298c57cb5f4bcc896f2acdd9018ba
JohnObama24 <johnobama24keren@gmail.com>
2024-09-18 22:45:52 +0700

    nambah animasi doank TwT, nambah masalah juga

--- lib/app/modules/sidebar/views/sidebar_view.dart
+++ lib/app/modules/sidebar/views/sidebar_view.dart
@@ -4,40 +4,38 @@ import 'package:construx_beta/app/modules/sidebar/controllers/sidebar_controller
 import '../../../../constanta/app_colors.dart';
 
 class Sidebar extends StatelessWidget {
-  final SidebarController controller = Get.put(SidebarController()); // Inisialisasi controller GetX untuk sidebar
+  final SidebarController controller = Get.put(SidebarController());
   Sidebar({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    //gilakk niat bet gua bikin komentar segala yakkk anying
     return Container(
       width: 300,
-      color: AppColors.abu, // Warna background sidebar
+      color: AppColors.abu,
       child: Column(
         children: [
-          _buildDrawerHeader(), // Bagian header sidebar
+          _buildDrawerHeader(),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: ListView(
                 padding: EdgeInsets.zero,
                 children: [
-                  _buildListTile('Home Page', Icons.home, 0, '/home'), // Item Home Page
+                  _buildListTile('Home Page', Icons.home, 0, '/home'),
                   _buildParentItem('Basic Settings', Icons.settings, 1, [
-                    _buildSubItem('Company Information', Icons.info, 1, '/company-information'), // Sub-item untuk Basic Settings :v
+                    _buildSubItem('Company Information', Icons.info, 1, '/company-information'),
                     _buildSubItem('User Role', Icons.person, 2, '/user-role'),
                     _buildSubItem('Commodity Category', Icons.category_outlined, 3, '/commodity-category'),
                   ]),
                   _buildListTile('Receiving Management', Icons.notifications, 2, '/receiving-management'),
                   _buildListTile('Stock Management', Icons.inventory_2, 3, '/stock-management'),
                   _buildParentItem('Statistic Analysis', Icons.analytics, 4, [
-                    _buildSubItem('Safety Stock', Icons.security, 4, '/safety-stock'), // Sub-item untuk Statistic Analysis :v
+                    _buildSubItem('Safety Stock', Icons.security, 4, '/safety-stock'),
                     _buildSubItem('Receiving Statistics', Icons.trending_up, 5, '/receiving-statistics'),
                     _buildSubItem('Shipment Statistics', Icons.local_shipping, 6, '/shipment-statistics'),
                   ]),
                   _buildParentItem('Warehouse Working', Icons.warehouse, 5, [
-                    _buildSubItem('Warehouse Processing', Icons.autorenew, 7, '/warehouse-processing'), // Sub-item untuk Warehouse Working :v
+                    _buildSubItem('Warehouse Processing', Icons.autorenew, 7, '/warehouse-processing'),
                     _buildSubItem('Inventory Move', Icons.move_to_inbox, 8, '/inventory-move'),
                     _buildSubItem('Inventory Freeze', Icons.ac_unit, 9, '/inventory-freeze'),
                     _buildSubItem('Inventory Adjust', Icons.adjust, 10, '/inventory-adjust'),
@@ -49,7 +47,7 @@ class Sidebar extends StatelessWidget {
               ),
             ),
           ),
-          _buildBackButton(), // Tombol back di bagian paling bawah 
+          _buildBackButton(),
         ],
       ),
     );
@@ -58,59 +56,73 @@ class Sidebar extends StatelessWidget {
   Widget _buildDrawerHeader() {
     return DrawerHeader(
       child: Center(
-        child: Image.asset('assets/images/CONSTRUX-LOGO.png', width: 400.0), // Logo di bagian header
+        child: Image.asset('assets/images/CONSTRUX-LOGO.png', width: 400.0),
       ),
     );
   }
 
   Widget _buildListTile(String title, IconData icon, int index, String route) {
-    return Obx(() => _buildContainer(
-          ListTile(
-            leading: Icon(icon, color: Colors.white), // Ikon di samping title
-            title: Text(title, style: const TextStyle(color: Colors.white)), // Title item
+    return Obx(() => AnimatedContainer(
+          duration: const Duration(milliseconds: 300), // Durasi animasi transisi warna
+          decoration: BoxDecoration(
+            color: controller.selectedIndex.value == index ? AppColors.hijau : AppColors.abu,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: ListTile(
+            leading: Icon(icon, color: Colors.white),
+            title: Text(title, style: const TextStyle(color: Colors.white)),
             onTap: () {
-              controller.selectItem(index); // Set item yang dipilih
-              controller.selectSubItem(-1); // Reset sub-item yang dipilih
-              controller.navigateWithDelay(route); // Navigasi dengan delay "lama bjir"
+              controller.selectItem(index);
+              controller.selectSubItem(-1);
+              controller.navigateWithDelay(route);
             },
           ),
-          index,
         ));
   }
 
   Widget _buildParentItem(String title, IconData icon, int index, List<Widget> subItems) {
     return Obx(() => Column(
           children: [
-            _buildContainer(
-              ListTile(
-                leading: Icon(icon, color: Colors.white), // Ikon untuk parent item
-                title: Text(title, style: const TextStyle(color: Colors.white)), // Title parent item
-                trailing: Icon(controller.isExpanded(index) ? Icons.expand_less : Icons.expand_more, color: Colors.white), // Ikon expand/collapse
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              decoration: BoxDecoration(
+                color: controller.selectedIndex.value == index ? AppColors.hijau : AppColors.abu,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: ListTile(
+                leading: Icon(icon, color: Colors.white),
+                title: Text(title, style: const TextStyle(color: Colors.white)),
+                trailing: Icon(controller.isExpanded(index) ? Icons.expand_less : Icons.expand_more, color: Colors.white),
                 onTap: () {
-                  controller.toggleExpand(index); // Toggle expand/collapse state
-                  controller.selectItem(index); // Set item utama yang dipilih
+                  controller.toggleExpand(index);
+                  controller.selectItem(index);
                 },
               ),
-              index,
             ),
-            if (controller.isExpanded(index)) ...subItems // Menampilkan sub-item jika parent item terbuka
+            AnimatedSize(
+              duration: const Duration(milliseconds: 300), // Durasi animasi untuk membuka/tutup sublist
+              child: Column(
+                children: controller.isExpanded(index) ? subItems : [],
+              ),
+            ),
           ],
         ));
   }
 
   Widget _buildSubItem(String title, IconData icon, int subIndex, String route) {
-    return Obx(() => Container(
-          margin: const EdgeInsets.only(left: 40, top: 5), // Margin untuk sub-item
+    return Obx(() => AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          margin: const EdgeInsets.only(left: 40, top: 5),
           decoration: BoxDecoration(
-            color: controller.selectedSubIndex.value == subIndex ? AppColors.putih : AppColors.abu, // Warna latar sub-item
-            borderRadius: BorderRadius.circular(8), // Border radius untuk sudut sub-item
+            color: controller.selectedSubIndex.value == subIndex ? AppColors.putih : AppColors.abu,
+            borderRadius: BorderRadius.circular(8),
           ),
           child: ListTile(
-            leading: Icon(icon, color: controller.selectedSubIndex.value == subIndex ? Colors.black : Colors.white), // Ikon untuk sub-item
-            title: Text(title, style: TextStyle(color: controller.selectedSubIndex.value == subIndex ? AppColors.textGelap : Colors.white)), // Title sub-item
+            leading: Icon(icon, color: controller.selectedSubIndex.value == subIndex ? Colors.black : Colors.white),
+            title: Text(title, style: TextStyle(color: controller.selectedSubIndex.value == subIndex ? AppColors.textGelap : Colors.white)),
             onTap: () {
-              controller.selectSubItem(subIndex); // Set sub-item yang dipilih
-              controller.navigateWithDelay(route); // Navigasi dengan delay
+              controller.selectSubItem(subIndex);
+              controller.navigateWithDelay(route);
             },
           ),
         ));
@@ -118,27 +130,17 @@ class Sidebar extends StatelessWidget {
 
   Widget _buildBackButton() {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16), // Padding tombol back agar tidak terlalu mepet
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
       child: ElevatedButton.icon(
-        onPressed: () => Get.back(), // Kembali ke halaman sebelumnya
-        icon: const Icon(Icons.arrow_back, color: AppColors.putih,),
-        label: const Text('Back', style: TextStyle(color: AppColors.putih),),
+        onPressed: () => Get.back(),
+        icon: const Icon(Icons.arrow_back, color: AppColors.putih),
+        label: const Text('Back', style: TextStyle(color: AppColors.putih)),
         style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.abuGelap, // Warna background tombol back
-          minimumSize: const Size.fromHeight(50), // Ukuran minimal tombol
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero), // Hapus border radius untuk tombol biar kotak anjayyy
+          backgroundColor: AppColors.abuGelap,
+          minimumSize: const Size.fromHeight(50),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
         ),
       ),
-    );
-  }
-
-  Widget _buildContainer(Widget child, int index) {
-    return Container(
-      decoration: BoxDecoration(
-        color: controller.selectedIndex.value == index ? AppColors.hijau : AppColors.abu, // Warna latar berdasarkan item yang dipilih
-        borderRadius: BorderRadius.circular(8.0), // Border radius untuk sudut item
-      ),
-      child: child,
     );
   }
 }
