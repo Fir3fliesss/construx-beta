c83efdf78141e207c772da4b0025654e1bc96d61
Fir3fliesss <gheraldymoses5@gmail.com>
2024-10-03 11:51:44 +0700

    stash

--- lib/app/modules/permission_settings/views/permission_settings_view.dart
+++ lib/app/modules/permission_settings/views/permission_settings_view.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import '../../../../constanta/app_colors.dart';
 
-import '../controllers/permission_settings_controller.dart';
-
-class PermissionSettingsView extends GetView<PermissionSettingsController> {
+class PermissionSettingsView extends StatelessWidget {
   const PermissionSettingsView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('PermissionSettingsView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'PermissionSettingsView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 32.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Basic Setting',
+                      style: TextStyle(
+                          fontSize: 24.0, fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Basic Settings > User Role',
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16.0,
+              )
+            ],
+          ))
+        ],
       ),
     );
   }
