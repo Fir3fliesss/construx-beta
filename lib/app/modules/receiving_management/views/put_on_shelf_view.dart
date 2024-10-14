ebf4a76f4f89b3b73756255870d64c172e7483dd
JohnObama24 <johnobama24keren@gmail.com>
2024-10-14 16:43:16 +0700

    style(receiving-management):addjust datatable anf checkbox in To be Put On the Shelf

--- lib/app/modules/receiving_management/views/put_on_shelf_view.dart
+++ lib/app/modules/receiving_management/views/put_on_shelf_view.dart
@@ -32,14 +32,13 @@ class PutOnShelfView extends StatelessWidget {
                             ),
                             Spacer(),
                             Text(
-                              'Receiving Management > Put On The Shelf',
+                              'Receiving Management > To Be Put On The Shelf',
                               style: TextStyle(color: Colors.black54),
                             ),
                           ],
                         ),
                       ),
                       const SizedBox(height: 16.0),
-
 
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -53,7 +52,8 @@ class PutOnShelfView extends StatelessWidget {
                           margin: const EdgeInsets.all(20.0),
                           padding: const EdgeInsets.all(5.0), // Updated padding
                           decoration: BoxDecoration(
-                            border: Border.all(color: AppColors.abuabu, width: 2.0),
+                            border:
+                                Border.all(color: AppColors.abuabu, width: 2.0),
                             borderRadius: BorderRadius.circular(5.0),
                           ),
                           child: Column(
@@ -62,11 +62,55 @@ class PutOnShelfView extends StatelessWidget {
                                 padding: const EdgeInsets.all(16.0),
                                 child: Row(
                                   children: [
-                                    buildCircleIconButton(Icons.add_circle_outline, 'Add', AppColors.abuabu),
+                                    buildCircleIconButton(
+                                        Icons.add_circle_outline,
+                                        'Add',
+                                        AppColors.abuabu),
                                     const SizedBox(width: 16),
-                                    buildCircleIconButton(Icons.refresh, 'Refresh', AppColors.abuabu),
+                                    buildCircleIconButton(Icons.refresh,
+                                        'Refresh', AppColors.abuabu),
                                     const SizedBox(width: 16),
-                                    buildCircleIconButton(Icons.upload_outlined, 'Upload', AppColors.abuabu),
+                                    buildCircleIconButton(Icons.upload_outlined,
+                                        'Upload', AppColors.abuabu),
+                                    const Spacer(),
+                                    Container(
+                                      width: 200,
+                                      height: 50,
+                                      child: TextField(
+                                        decoration: InputDecoration(
+                                          hintText: 'Supplier Name',
+                                          hintStyle: TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 16,
+                                          ),
+                                          border: OutlineInputBorder(),
+                                          contentPadding: EdgeInsets.all(12.0),
+                                        ),
+                                        textAlignVertical:
+                                            TextAlignVertical.center,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 16),
+                                    Container(
+                                      width: 200,
+                                      height: 50,
+                                      child: TextField(
+                                        decoration: InputDecoration(
+                                          hintText: 'Form Name',
+                                          hintStyle: TextStyle(
+                                            color: Colors
+                                                .grey, // Kamu bisa ubah warna atau gaya teks hint di sini
+                                            fontSize:
+                                                16, // Sesuaikan ukuran font
+                                          ),
+                                          border: OutlineInputBorder(),
+                                          contentPadding: EdgeInsets.all(
+                                              12.0), // Tetap penting untuk menyesuaikan padding
+                                        ),
+                                        textAlignVertical: TextAlignVertical
+                                            .center, // Pastikan teks di tengah vertikal
+                                      ),
+                                    ),
                                   ],
                                 ),
                               ),
@@ -81,34 +125,42 @@ class PutOnShelfView extends StatelessWidget {
                                   child: Padding(
                                     padding: const EdgeInsets.all(20.0),
                                     child: DataTable(
-                                      headingRowColor: WidgetStateProperty.all(Colors.grey[200]),
-                                      columnSpacing: 10, // Adjust column spacing
-                                      columns: [
-                                        DataColumn(label: Text('')),
-                                        DataColumn(label: Text('No')),
-                                        DataColumn(label: Text('Asn No')),
-                                        DataColumn(label: Text('Batch')),
-                                        DataColumn(label: Text('Estimated time of arrival')),
-                                        DataColumn(label: Text('Good Owner Name')),
-                                        DataColumn(label: Text('Operate')),
-                                      ],
-                                      rows: List.generate(
-                                        15,
-                                        (index) => DataRow(cells: [
-                                          DataCell(Checkbox(value: false, onChanged: (bool? value) {})),
-                                          DataCell(Text('${index + 1}')),
-                                          DataCell(Text('20240824-0003')),
-                                          DataCell(Text(index < 2 ? '${333 + index}' : '3433')),
-                                          DataCell(Text('2024-08-1')),
-                                          DataCell(Text('-')),
-                                          DataCell(Row(
-                                            children: [
-                                              Icon(Icons.edit, color: Colors.blue),
-                                              SizedBox(width: 10),
-                                              Icon(Icons.delete, color: Colors.red),
-                                            ],
-                                            ),
-                                          ),
+                              headingRowColor: MaterialStateProperty.all(Colors.grey[200]),
+                              columnSpacing: 10,
+                              columns: [
+                              DataColumn(label: Text('No', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('')),
+                              DataColumn(label: Text('ASN No', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Commodity\nCode', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Trade Name', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Specification\nCode', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('From\nName', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Goods Owner\nName', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Supplier\nName', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Asn\nQuantity', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Commodity\nPrice', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Weight', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Volume', style: TextStyle(fontSize: 12))),
+                              DataColumn(label: Text('Operate', style: TextStyle(fontSize: 12))),
+                            ],
+                            rows: List.generate(
+                              10,
+                              (index) => DataRow(cells: [
+                                DataCell(Text('${index + 1}', style: TextStyle(fontSize: 12))),
+                                DataCell(Checkbox(value: false, onChanged: (bool? value) {})),
+                                DataCell(Text('20240731-0001', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('20240731-0001', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Text('-', style: TextStyle(fontSize: 12))),
+                                DataCell(Icon(Icons.share_outlined, color: AppColors.textGelap, size: 16)),
+                  
                                         ]),
                                       ),
                                     ),
